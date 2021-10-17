97ee5695ff39ee6264b75501efe25de5853fa377
Otchi <adamgragnic@gmail.com>
2021-10-17 21:23:53 +0200

    Tentative de connexion à l'API Digital Ocean

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,23 @@
 import { Button, Container, TextField } from '@mui/material';
 import React from 'react';
 import Navigation from '../components/Navigation';
+import Axios from 'axios'
 
-const login = (e) => {
-    console.log(e)
+const Attempt = () => {
+    console.log("Login")
 }
 
-const Test = () => {
+const Login = () => {
+    Axios.get('otchi.games:8000/')
+            .then((res) => {
+                console.log(res)
+            })
 
     return (
         <div className="page">
             <Navigation/>
             <Container sx={{ p:"5%"}} maxWidth="xs">
-                <form style={{display:"flex", flexDirection:"column"}} onSubmit={login}>
+                <form style={{display:"flex", flexDirection:"column"}} onSubmit={Attempt}>
                     <TextField sx={{marginY:"1rem"}} variant="standard" label="Username"/>
                     <TextField sx={{marginY:"1rem"}} variant="standard" type="password" label="Password"/>
                     <Button type="submit" sx={{marginY:"1rem"}}>Se connecter</Button>
@@ -22,4 +27,4 @@ const Test = () => {
     );
 };
 
-export default Test;
+export default Login;
