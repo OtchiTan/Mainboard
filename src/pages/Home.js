f4b8e270ee29ae45fdbaffce7069ce7b771c8aad
Adam <adamgragnic@gmail.com>
2021-10-19 19:15:40 +0200

    Tentative de lien avec l'API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,11 +8,16 @@ const Home = () => {
         <Box>
             <Navigation/>
 
-            <Container maxWidth="md" sx={{p:"5%", display:"grid", gap:1, gridTemplate:"repeat(2,1fr) / repeat(3,1fr)"}}>
+            <Container maxWidth="md" sx={{
+                    p:"5%",
+                    display:"grid",
+                    gap:1,
+                    gridTemplate:"repeat(2,1fr) / repeat(3,1fr)"
+                }}>
                 <Button variant="outlined" href="http://pma.otchi.games">PhpMyAdmin</Button>
                 <Button variant="outlined" href="http://portfolio.otchi.games">Portfolio</Button>
-                <Button variant="outlined" href="">Github</Button>
-                <Button variant="outlined" href="">Gitlab</Button>
+                <Button variant="outlined" href="https://github.com/OtchiTan">Github</Button>
+                <Button variant="outlined" href="https://gitlab.com/OtchiTan">Gitlab</Button>
                 <Button variant="outlined">...</Button>
                 <Button variant="outlined">...</Button>
             </Container>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,30 +1,51 @@
 import { Button, Container, TextField } from '@mui/material';
-import React from 'react';
 import Navigation from '../components/Navigation';
 import Axios from 'axios'
-
-const Attempt = () => {
-    console.log("Login")
-}
+import { useState } from 'react';
 
 const Login = () => {
-    Axios.get('otchi.games:8000/')
-            .then((res) => {
-                console.log(res)
-            })
+    const [refLogin,setRefLogin] = useState("")
+    const [refPwd,setRefPwd] = useState("")
+    const Attempt = () => {
+        const body = {
+            login:refLogin,
+            pwd:refPwd
+        }
+        /*Axios.post('localhost:8000',body)
+                .then((res) => {
+                    console.log(res)
+                })*/
+        Axios({
+            method:'POST',
+            url:'http://localhost:8000',
+            data:body
+        })
+    }
+    const loginChangeHandler = (event) => {
+        setRefLogin(event.target.value)
+    }
+    const pwdChangeHandler = (event) => {
+        setRefPwd(event.target.value)
+    }  
 
     return (
         <div className="page">
             <Navigation/>
             <Container sx={{ p:"5%"}} maxWidth="xs">
                 <form style={{display:"flex", flexDirection:"column"}} onSubmit={Attempt}>
-                    <TextField sx={{marginY:"1rem"}} variant="standard" label="Username"/>
-                    <TextField sx={{marginY:"1rem"}} variant="standard" type="password" label="Password"/>
+                    <TextField sx={{marginY:"1rem"}} variant="standard" label="Username" value={refLogin} onChange={loginChangeHandler}/>
+                    <TextField sx={{marginY:"1rem"}} variant="standard" type="password" label="Password" value={refPwd} onChange={pwdChangeHandler}/>
                     <Button type="submit" sx={{marginY:"1rem"}}>Se connecter</Button>
                 </form>     
             </Container>
         </div>
     );
 };
+/*
+
+                    <TextField sx={{marginY:"1rem"}} variant="standard" label="Username" ref={refLogin}/>
+                    <TextField sx={{marginY:"1rem"}} variant="standard" type="password" label="Password" ref={refPwd}/>
+                    <Button type="submit" sx={{marginY:"1rem"}}>Se connecter</Button>
+*/
 
 export default Login;
